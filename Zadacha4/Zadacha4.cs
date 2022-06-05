f25a2005633dbc021f7f542af850cd510fd2345e
«Alexndr» <alex.bat@bk.ru>
2022-06-05 21:27:08 +0300

    add Zadacha 4

---
+++ Zadacha4/Zadacha4.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите три числа");
+Console.WriteLine("Первое число: ");
+int na = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int nb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число: ");
+int nc = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (na > nb)
+{
+    max = na;
+}
+else 
+{
+    max = nb;
+}
+if (nc > max)
+{
+    max = nc;
+}
+Console.WriteLine("Максимальное число: ");
+Console.WriteLine(max);
