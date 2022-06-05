2caee307c35496a127cf4f0f75c2564bb19df7ec
«Alexndr» <alex.bat@bk.ru>
2022-06-05 22:14:45 +0300

    add Zadacha 8

---
+++ Zadacha8/Zadacha8.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+Console.WriteLine("Четные числа в диапазоне от 1 до Вашего числа: ");
+while(i<=n)
+{
+    Console.WriteLine(i);
+    i = i + 2;
+}
