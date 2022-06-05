13b397b9912a648cac9685c0d8e174503da7c1d3
«Alexndr» <alex.bat@bk.ru>
2022-06-05 20:27:43 +0300

    add Zadacha 2

---
+++ Zadacha2/Zadacha2.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два числа: ");
+int na = Convert.ToInt32(Console.ReadLine()), 
+    nb = Convert.ToInt32(Console.ReadLine());
+
+if (na > nb)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(na);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(nb);
+}
+else if (na < nb)
+{
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(na);
+    Console.Write("Большее число: ");
+    Console.WriteLine(nb);
+}
